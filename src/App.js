62770d76b52912dc73ed0a54c262bcd49779aea0
Akshat Jha <akshat.k.jha@gmail.com>
2023-03-25 19:33:14 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
-// import About from './components/About';
+import About from './components/About';
 import React, {useState} from 'react'
 import Alert from './components/Alert';
-// import {Routes, Route} from 'react-router-dom';
+import {Routes, Route} from 'react-router-dom';
 
 function App() {
   const [mode, setMode] = useState('light'); // whether dark mode is enabled or not
@@ -22,13 +22,13 @@ function App() {
       setMode('dark');
       document.body.style.backgroundColor = '#042743';
       showAlert("Dark mode has been enabled", "success");
-      document.title = "TextUtils - Dark Mode";
+      // document.title = "TextUtils - Dark Mode";
     }
     else {
       setMode('light');
       document.body.style.backgroundColor = 'white';
       showAlert("Light mode has been enabled", "success");
-      document.title = "TextUtils - Light Mode";
+      // document.title = "TextUtils - Light Mode";
     }
   }
   return (
@@ -37,16 +37,10 @@ function App() {
       <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
       <Alert alert={alert}/>
       <div className="container my-3">
-          {/* <Routes>
-              <Route path="/about">
-                <About />
-              </Route>
-              <Route path="/"> */}
-                <TextForm showAlert={showAlert} heading="Enter the text to analyze" mode={mode}/>
-              {/* </Route>
-              <Route path="/about" element={<About/>}/>
+          <Routes>
+              <Route path="/about" element={<About mode={mode}/>}/>
               <Route path="/" element={<TextForm showAlert={showAlert} heading="Enter the text to analyze" mode={mode}/>}/>
-          </Routes> */}
+          </Routes>
       </div>
     </>
   );
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -37,17 +37,17 @@ export default function TextForm(props) {
                 <div className="mb-3">
                     <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode==='dark'?'grey':'white', color: props.mode==='dark'?'white':'#042743'}} id="myBox" rows="8"></textarea>
                 </div>
-                <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to uppercase</button>
-                <button className="btn btn-primary mx-1" onClick={handleLoClick}>Convert to lowercase</button>
-                <button className="btn btn-primary mx-1" onClick={handleClearClick}>Clear</button>
-                <button className="btn btn-warning mx-1" onClick={speak}>Speak</button>
+                <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleUpClick}>Convert to uppercase</button>
+                <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleLoClick}>Convert to lowercase</button>
+                <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleClearClick}>Clear</button>
+                <button disabled={text.length===0} className="btn btn-warning mx-1 my-1" onClick={speak}>Speak</button>
             </div>
             <div className="container my-3" style={{color: props.mode==='dark'?'white':'#042743'}}>
                 <h3>Your text summary</h3>
-                <p>{text.split(" ").length} words and {text.length} characters</p>
-                <p>{0.008*text.split(" ").length} Minutes read</p>
+                <p>{text.split(/\s+/).filter((element)=>{return element.length!==0}).length} words and {text.length} characters</p>
+                <p>{0.008*text.split(" ").filter((element)=>{return element.length!==0}).length} Minutes read</p>
                 <h2>Preview</h2>
-                <p>{text.length>0?text:"Enter something to preview"}</p>
+                <p>{text.length>0?text:"Nothing to preview"}</p>
             </div>
         </>
     )
